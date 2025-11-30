5a7eed2bd7469c590ab52649f3d053c2d26ace09
merces <nandu88@gmail.com>
2025-11-30 00:26:21 -0300

    Light theme

--- src/themes.rs
+++ src/themes.rs
@@ -2,7 +2,7 @@ use ratatui::style::{Color, Modifier, Style};
 
 pub struct Theme {
     pub main: Style,
-    pub zeroes: Style,
+    pub dimmed: Style,
     pub offsets: Style,
     pub changed_bytes: Style,
     pub highlight: Style,
@@ -13,7 +13,7 @@ pub struct Theme {
     pub dialog: Style,
 }
 
-pub const VSCODE: Theme = Theme {
+pub const DARK: Theme = Theme {
     offsets: Style::new()
         .fg(Color::from_u32(0x569cd6))
         .bg(Color::from_u32(0x1e1e1e))
@@ -22,7 +22,7 @@ pub const VSCODE: Theme = Theme {
         .fg(Color::from_u32(0xd4d4d4))
         .bg(Color::from_u32(0x1e1e1e))
         .add_modifier(Modifier::BOLD),
-    zeroes: Style::new()
+    dimmed: Style::new()
         .fg(Color::from_u32(0x949494))
         .bg(Color::from_u32(0x1e1e1e))
         .add_modifier(Modifier::BOLD),
@@ -46,5 +46,52 @@ pub const VSCODE: Theme = Theme {
     editing: Style::new()
         .fg(Color::from_u32(0x1e1e1e))
         .bg(Color::Rgb(255, 215, 0))
+        .add_modifier(Modifier::RAPID_BLINK),
+};
+
+pub const LIGHT: Theme = Theme {
+    offsets: Style::new()
+        .fg(Color::from_u32(0x237893))
+        .bg(Color::from_u32(0xffffff))
+        .add_modifier(Modifier::BOLD),
+
+    main: Style::new()
+        .fg(Color::from_u32(0x000000))
+        .bg(Color::from_u32(0xffffff))
+        .add_modifier(Modifier::BOLD),
+
+    dimmed: Style::new()
+        .fg(Color::from_u32(0xa0a0a0))
+        .bg(Color::from_u32(0xffffff))
+        .add_modifier(Modifier::BOLD),
+
+    dialog: Style::new()
+        .fg(Color::from_u32(0x333333))
+        .bg(Color::from_u32(0xe7f3ff))
+        .add_modifier(Modifier::BOLD),
+
+    changed_bytes: Style::new()
+        .fg(Color::from_u32(0x795e00))
+        .bg(Color::from_u32(0xffffff)),
+
+    highlight: Style::new()
+        .fg(Color::from_u32(0x000000))
+        .bg(Color::from_u32(0xadd6ff)),
+
+    byte_highlight: Style::new()
+        .fg(Color::Black)
+        .bg(Color::from_u32(0xffb3b3)),
+
+    topbar: Style::new()
+        .fg(Color::from_u32(0x333333))
+        .bg(Color::from_u32(0xf3f3f3)),
+
+    error: Style::new()
+        .fg(Color::from_u32(0xe51400))
+        .bg(Color::from_u32(0xf2dede)),
+
+    editing: Style::new()
+        .fg(Color::from_u32(0xffffff))
+        .bg(Color::from_u32(0xffcc00))
         .add_modifier(Modifier::RAPID_BLINK),
 };
