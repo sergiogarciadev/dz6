708da6bcf467459625ca4019f0714158981399de
merces <nandu88@gmail.com>
2025-12-06 22:10:26 -0300

    Remove existing database if there is nothing to be saved

--- src/database.rs
+++ src/database.rs
@@ -6,17 +6,19 @@ use crate::app::App;
 
 impl App {
     pub fn save_database(&self) -> Result<(), Box<dyn Error>> {
-        // do not save a database if there's nothing to be saved
-        if self.hex_view.bookmarks.is_empty() && self.hex_view.comment_name_list.is_empty() {
-            return Ok(());
-        }
-
         let toml_string = toml::to_string_pretty(&self.hex_view)?;
         let target_dir: &Path = Path::new(&self.file_info.path)
             .parent()
             .unwrap_or(Path::new("."));
         let cwd_db = format!("{}.dz6", self.file_info.name);
         let target_db: PathBuf = target_dir.join(&cwd_db);
+
+        // if there's nothing to be saved, delete any existing db files and return
+        if self.hex_view.bookmarks.is_empty() && self.hex_view.comment_name_list.is_empty() {
+            let _ = fs::remove_file(target_db);
+            let _ = fs::remove_file(cwd_db);
+            return Ok(());
+        }
 
         // try target's path or else current directory
         fs::write(&target_db, &toml_string).or_else(|_| fs::write(&cwd_db, &toml_string))?;
