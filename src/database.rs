08729b207bf0921ec7bc00fa9a28921559178604
merces <nandu88@gmail.com>
2025-12-06 21:20:51 -0300

    Improve database file logic

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fs;
+use std::path::{Path, PathBuf};
 
 use crate::app::App;
 
@@ -11,13 +12,21 @@ impl App {
         }
 
         let toml_string = toml::to_string_pretty(&self.hex_view)?;
-        let dbname = self.file_info.name.clone() + ".dz6";
-        fs::write(dbname, toml_string)?;
+        let target_dir: &Path = Path::new(&self.file_info.path).parent().unwrap_or(Path::new("."));
+        let cwd_db = format!("{}.dz6", self.file_info.name);
+        let target_db: PathBuf = target_dir.join(&cwd_db);
+
+        // try target's path or else current directory
+        fs::write(&target_db, &toml_string).or_else(|_| fs::write(&cwd_db, &toml_string))?;
+
         Ok(())
     }
     pub fn load_database(&mut self) -> Result<(), Box<dyn Error>> {
-        let dbname = self.file_info.name.clone() + ".dz6";
-        let data = fs::read_to_string(dbname)?;
+        let target_dir: &Path = Path::new(&self.file_info.path).parent().unwrap_or(Path::new("."));
+        let cwd_db = format!("{}.dz6", self.file_info.name);
+        let target_db: PathBuf = target_dir.join(&cwd_db);
+        let data = fs::read_to_string(&cwd_db).or_else(|_| fs::read_to_string(&target_db))?;
+
         self.hex_view = toml::from_str(&data)?;
         Ok(())
     }
